272d3999eb2b03c07badacb1c667c7b94a8a018a
Prithvi-boi <prithvi.bel13@gmail.com>
2026-07-30 19:18:00 +0530

    useRef practice

--- ReactLearning/src/App.jsx
+++ ReactLearning/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { useConsoleLogger, useToggle } from "./customHooks"
 
 function App() {
   const [value, setvalue] = useState('')
   useConsoleLogger(value)
+  const btnref = useRef()
+  console.log(btnref.current)
   
   const [bgColortoggle, settoggle] = useToggle()
 
@@ -13,7 +15,7 @@ function App() {
       <input type="text" placeholder="Type anything" onChange={(e) => {setvalue(e.target.value)}}></input>
       <hr />
       <div className={`${bgColortoggle ? 'bg-gray-600' : 'bg-black'} h-20 w-full`}>
-        <button className="h-10 w-20 bg-white" onClick={() => settoggle()}>Change Bg color</button>
+        <button ref={btnref} style={{fontSize: `${bgColortoggle ? 10 : 20}px`}} className="h-10 w-20 bg-white" onClick={() => settoggle()}>Change Bg color</button>
       </div>    
     </>
   )
